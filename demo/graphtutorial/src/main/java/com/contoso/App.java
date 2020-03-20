d3bbac13783b58ce00dc538c39118a02e973d54e
Jason Johnston <jasonjoh@users.noreply.github.com>
2020-03-20 16:57:35 -0400

    Completed add MS Graph step

--- demo/graphtutorial/src/main/java/com/contoso/App.java
+++ demo/graphtutorial/src/main/java/com/contoso/App.java
@@ -5,9 +5,17 @@ package com.contoso;
 
 import java.io.InputStream;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
+
+import com.microsoft.graph.models.extensions.DateTimeTimeZone;
+import com.microsoft.graph.models.extensions.Event;
+import com.microsoft.graph.models.extensions.User;
 
 /**
  * Graph Tutorial
@@ -38,6 +46,11 @@ public class App {
         Authentication.initialize(appId);
         final String accessToken = Authentication.getUserAccessToken(appScopes);
 
+        // Greet the user
+        User user = Graph.getUser(accessToken);
+        System.out.println("Welcome " + user.displayName);
+        System.out.println();
+
         Scanner input = new Scanner(System.in);
 
         int choice = -1;
@@ -67,12 +80,42 @@ public class App {
                     break;
                 case 2:
                     // List the calendar
+                    listCalendarEvents(accessToken);
                     break;
                 default:
                     System.out.println("Invalid choice");
             }
         }
 
+        Graph.shutdown();
         input.close();
     }
+
+    // <FormatDateTimeSnippet>
+    private static String formatDateTimeTimeZone(DateTimeTimeZone date) {
+        LocalDateTime dateTime = LocalDateTime.parse(date.dateTime);
+
+        return dateTime.format(
+            DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)) +
+            " (" + date.timeZone + ")";
+    }
+    // </FormatDateTimeSnippet>
+
+    // <ListEventsSnippet>
+    private static void listCalendarEvents(String accessToken) {
+        // Get the user's events
+        List<Event> events = Graph.getEvents(accessToken);
+
+        System.out.println("Events:");
+
+        for (Event event : events) {
+            System.out.println("Subject: " + event.subject);
+            System.out.println("  Organizer: " + event.organizer.emailAddress.name);
+            System.out.println("  Start: " + formatDateTimeTimeZone(event.start));
+            System.out.println("  End: " + formatDateTimeTimeZone(event.end));
+        }
+
+        System.out.println();
+    }
+    // </ListEventsSnippet>
 }
