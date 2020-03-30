5a88c647f462a07e44caf1a2358b97ebcc15377b
Jason Johnston <jasonjoh@users.noreply.github.com>
2020-03-30 13:42:15 -0400

    Update AppTest.java

--- demo/graphtutorial/src/test/java/graphtutorial/AppTest.java
+++ demo/graphtutorial/src/test/java/graphtutorial/AppTest.java
@@ -7,8 +7,5 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
+
 }
